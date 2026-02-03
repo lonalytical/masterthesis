cd3fb6aeb86f7553bca8126195e19536afd268be
Lona Lisa <lona.friessner@gmail.com>
2026-02-03 18:09:45 +0100

    new nested facets for plot

--- code/bias-plot_script.R
+++ code/bias-plot_script.R
@@ -145,3 +145,44 @@ plotbias60 <- ggplot(results_bias60, aes(x = ID, y = bias, fill = method)) +
 #   scale_y_continuous(limits = c(-0.10, 0.05)) +
 #   theme_minimal(base_size = 10)
 # 
+
+################ Alternative ##############
+
+results <- read.table(file = here("results", "summarized_simulation-results.csv"))
+
+biasd <- results %>%
+  filter(parameter == "gamma01",
+         method %in% c("CD", "LD", "MI-R", "bayes"),
+         beta == 0.3) %>%
+  mutate(
+    lower = bias - 2 * mcse_bias,
+    upper = bias + 2 * mcse_bias,
+    method = factor(method, levels = c("CD", "LD", "MI-R", "bayes")),
+    ID = factor(ID)
+  )
+library(ggh4x)
+
+ggplot(biasd, aes(x = method, y = bias, fill = method)) +
+  geom_col(width = 0.7)+
+  geom_errorbar(aes(ymin = lower, ymax = upper), width = 0.2) +
+  facet_nested(
+    rows = vars(N2, gamma01),
+    cols = vars(ICC),
+    labeller = labeller(
+      N2 = \(x) paste0("N2 = ", x),
+      gamma01 = \(x) paste0("ES = ", x),
+      ICC = \(x) paste0("ICC = ", x)
+    )
+  ) +
+  geom_hline(yintercept = 0) +
+  labs(
+    title = bquote("Bias for" ~ gamma["01"]),
+    x = "",
+    y = "Absolute bias (± 2×MCSE)",
+    fill = "Method"
+  )+
+  theme_grey()+
+  theme(
+    panel.border = element_rect(colour = "black", fill = NA, linewidth = 0.5),
+    panel.spacing = unit(0.8, "lines")    # etwas Luft zwischen Panels
+  )
