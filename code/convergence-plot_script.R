a19e054b520f025f724373e4aa65837ad88b66bf
Lona Lisa <lona.friessner@gmail.com>
2026-03-23 14:44:04 +0100

    worked on discussion

--- code/convergence-plot_script.R
+++ code/convergence-plot_script.R
@@ -6,6 +6,8 @@ library(coda)
 imp<-readRDS(here("results", "imp_convergence.rds"))
 #summary(imp)
 miplot<-plot(imp, print = "psi", pos=c(1,1),trace="imputation")
+#NOTE: This plot is not rendered in Quarto, so it had to be saved here
+
 
 #### BAYES
 fit.bayes<-readRDS(here("results", "fit_bayes_convergence.rds"))
@@ -18,5 +20,5 @@ acfplot(fit.bayes$values_coda[, "Y_vcov_group_(Intercept)-(Intercept)"])
 traceplot(fit.bayes$values_coda[, "x_vcov_group_(Intercept)-(Intercept)"])
 acfplot(fit.bayes$values_coda[, "x_vcov_group_(Intercept)-(Intercept)"])
 
-x <- fit.bayes$values_coda[, "Y_beta_gm(x, group)"]
+
 
